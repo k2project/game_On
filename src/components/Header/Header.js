dcc4360394adb8f35355b5f0a2722e2ba940bdff
k2project <kopczkrzy@gmail.com>
2019-10-20 19:28:21 +0100

    accessibility: menu icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,15 @@ export default function Header(){
         <header className="Header anchor">
             <div className="header__title">WEBDEVELOPER  WITH BALLS</div>
             <div className="header__mail">
-                 <img src={require('../../imgs/icons/mailOutline.png')}  alt="mail"/>
+                 <img src={require('../../imgs/icons/mailOutline.png')}  alt=""/>
                  <span> kopczkrzy@gmail.com</span>
 
              </div>
             <a href="https://twitter.com/_k2project" target="_blank" rel="noopener noreferrer">
-                <img src={require('../../imgs/icons/twitterOutline.png')}  alt="Twitter"/>
+                <img src={require('../../imgs/icons/twitterOutline.png')}  alt="Twitter account opens in a new window"/>
             </a>
             <a href="https://github.com/k2project" target="_blank" rel="noopener noreferrer">
-                <img src={require('../../imgs/icons/githubOutline.png')}  alt="GitHub"/>
+                <img src={require('../../imgs/icons/githubOutline.png')}  alt="GitHub account opens up in a new window"/>
             </a>
         </header>
     )
